a98b384a492b4a06cc41af2fd06464a5ca0369aa
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-05-15 19:15:56 -0300

    update de files name

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-const app = require('./main')
+const express = require('express');
+const app = express();
 
-app.listen(3000, () =>{
-    console.log(`Servidor rotando em http://localhost:3000`);    
-    
+app.set('view engine', 'ejs');
+
+app.get('/', (req, resp)=>{
+    resp.render('home/home')
 });
+
+app.get('/produtos', (req, resp) =>{
+    
+    const livros = [
+        {
+            titulo: 'Android',
+            preco: 'R$ 10,00',
+            descricao: 'Livro para aprender Android'
+        },
+        {
+            titulo: 'Node',
+            preco: 'R$ 25,00',
+            descricao: 'Livro para aprender NodeJs'
+        }
+    ]
+    
+    resp.render('produtos/lista', {livros: livros})
+});
+
+
+app.use(express.static('./public'));
+
+app.use((req, resp) => {
+    resp
+        .status(404)
+        .render('erros/erro', {erro: '404 - Página não encontrada'})
+});
+
+module.exports = app;
